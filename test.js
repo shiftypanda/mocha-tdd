d9b5cea0b9adcddd768b2354e63b2af19d370566
Shiftypanda <drtimddavis@gmail.com>
2018-05-30 20:44:14 +0100

    added unit testing for addition of minus numbers

--- test.js
+++ test.js
@@ -8,3 +8,7 @@ it('testing package working and should return true', () =>{
 it('correctly calculates the sum of 1 and 3', () => {
   assert.equal(operations.add(1, 3), 4);
 })
+
+it('correctly calcultes the sum of -1 and -1', () => {
+  assert.equal(operations.add(-1, -1), -2);
+})
