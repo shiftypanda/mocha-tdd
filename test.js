b0aefa1113276b79f7e9928d22e769926cc5b4ac
Shiftypanda <drtimddavis@gmail.com>
2018-05-30 20:38:05 +0100

    first unit test for add operation created - failing as expected

--- test.js
+++ test.js
@@ -3,3 +3,7 @@ const assert = require('assert');
 it('testing package working and should return true', () =>{
   assert.equal(true, true);
 })
+
+it('correctly calculates the sum of 1 and 3', () => {
+  assert.equal(add(1, 3), 4);
+})
