914256924fe808826321bf80aaf3b2543a7afa1e
Shiftypanda <drtimddavis@gmail.com>
2018-05-30 20:56:52 +0100

    added validateNumbers function unit tests passing

--- operations.js
+++ operations.js
@@ -3,9 +3,17 @@ const subtract = (x, y) => (+x) - (+y);
 const multiply = (x, y) => (+x) * (+y);
 const divide = (x, y) => (+x) / (+y);
 
+const validateNumbers = (x, y) => {
+  if (isNaN(x) || isNaN(y)) {
+    return false;
+  }
+  return true;
+}
+
 module.exports = {
   add,
   subtract,
   multiply,
   divide,
+  validateNumbers,
 }
--- test.js
+++ test.js
@@ -4,7 +4,7 @@ const assert = require('assert');
 it('testing package working and should return true', () =>{
   assert.equal(true, true);
 })
-
+// Checks calculation functions of operations.js
 it('correctly calculates the sum of 1 and 3', () => {
   assert.equal(operations.add(1, 3), 4);
 })
@@ -23,4 +23,17 @@ it('correctly calculates the product of 12 and 12', () => {
 
 it('correclty calcualtes the quotient of 10 and 2', () => {
   assert.equal(operations.divide(10, 2), 5);
+})
+
+// checks validation tehniques within operations.js
+it('indicates failure whena  string is used instead of a number', () => {
+  assert.equal(operations.validateNumbers('sammy', 5), false);
+})
+
+it('indicates failure when two strings are used instead of numbers', () => {
+  assert.equal(operations.validateNumbers('sammy', 'dave'), false);
+})
+
+it('succesfuly runs when two numbers are used', () => {
+  assert.equal(operations.validateNumbers(5, 5), true);
 })
